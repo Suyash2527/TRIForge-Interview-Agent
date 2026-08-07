be1237b8ec12023cd3acb01782eb6f59ca7eb7e0
Suyash2527~ <csuyash2506@gmail.com>
2026-08-07 23:08:30 +0530

    feat: add 3D particle sparkles to background

--- src/components/BackgroundLiquid.tsx
+++ src/components/BackgroundLiquid.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Sphere, MeshDistortMaterial } from '@react-three/drei';
+import { Sphere, MeshDistortMaterial, Sparkles } from '@react-three/drei';
 import * as THREE from 'three';
 
 function LiquidShape() {
@@ -33,11 +33,12 @@ function LiquidShape() {
 
 export default function BackgroundLiquid() {
   return (
-    <div className="fixed inset-0 -z-10 w-full h-full pointer-events-none opacity-30 mix-blend-screen">
+    <div className="fixed inset-0 -z-10 w-full h-full pointer-events-none opacity-40 mix-blend-screen">
       <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 10]} intensity={1} color="#7C5CFC" />
         <directionalLight position={[-10, -10, -10]} intensity={0.5} color="#4F8CFF" />
+        <Sparkles count={800} scale={12} size={1.2} speed={0.2} opacity={0.3} color="#4F8CFF" />
         <LiquidShape />
       </Canvas>
     </div>
